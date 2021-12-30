b4e66210eacc6d43f176250c3f97f8503c39fb11
sanjay <sanjay@orcalabs.tech>
2021-12-30 11:55:54 +0530

    main.tf file modified

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
   access_key = "AKIA5Q2KYCR3IJ23GIW5"
